1dbb497de3a321ca1c0036903f4f03746c0fab39
avinashgbhosale <bhosaleavinash22@gmail.com>
2020-01-05 15:20:17 +0530

    Avinash: Changes for app.py removed .pyc

--- app.py
+++ app.py
@@ -9,21 +9,3 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config['ERROR_404_HELP'] = False
 api.init_app(app)
-
-"""
-@app.after_request
-def after_request(response):
-    data = response.get_json()
-    if type(data) is dict:
-        print("it's dict")
-        '''
-        if 'statusCode' not in data:
-            data['statusCodes'] = 200
-        '''
-        print(data)
-        response.data = json.dumps(data)
-
-    return response
-"""
-
-app.run(debug=True)
